ba2fd4d09eabab36625f496f7d38c6c5274b24ea
Matthew J. Mucklo <mmucklo@gmail.com>
2020-07-03 09:58:44 -0700

    Address issue #24

--- Twig/Extension/TwigExtensionLegacy.php
+++ Twig/Extension/TwigExtensionLegacy.php
@@ -2,53 +2,55 @@
 
 namespace Dtc\GridBundle\Twig\Extension;
 
-class TwigExtension extends \Twig_Extension
-{
-    public function getFunction()
+if (!class_exists('Twig\Extension\AbstractExtension')) {
+    class TwigExtension extends \Twig_Extension
     {
-        $names = array(
-            'format_cell' => 'format_cell',
-        );
+        public function getFunction()
+        {
+            $names = array(
+                'format_cell' => 'format_cell',
+            );
 
-        $funcs = array();
-        foreach ($names as $twig => $local) {
-            $funcs[$twig] = new \Twig_SimpleFunction($twig, [$this, $local]);
-        }
-
-        return $funcs;
-    }
-
-    public function getFilters()
-    {
-        $names = array(
-            'format_cell' => 'format_cell',
-        );
-
-        $funcs = array();
-        foreach ($names as $twig => $local) {
-            $funcs[$twig] = new \Twig_SimpleFilter($twig, [$this, $local]);
-        }
-
-        return $funcs;
-    }
-
-    public function getName()
-    {
-        return 'dtc_grid';
-    }
-
-    public function format_cell($value)
-    {
-        if (is_object($value)) {
-            if ($value instanceof \DateTime) {
-                return $value->format(\DateTime::ISO8601);
+            $funcs = array();
+            foreach ($names as $twig => $local) {
+                $funcs[$twig] = new \Twig_SimpleFunction($twig, [$this, $local]);
             }
 
-            return 'object: '.get_class($value);
-        } elseif (is_scalar($value)) {
-            return $value;
-        } elseif (is_array($value)) {
-            return 'array';
+            return $funcs;
+        }
+
+        public function getFilters()
+        {
+            $names = array(
+                'format_cell' => 'format_cell',
+            );
+
+            $funcs = array();
+            foreach ($names as $twig => $local) {
+                $funcs[$twig] = new \Twig_SimpleFilter($twig, [$this, $local]);
+            }
+
+            return $funcs;
+        }
+
+        public function getName()
+        {
+            return 'dtc_grid';
+        }
+
+        public function format_cell($value)
+        {
+            if (is_object($value)) {
+                if ($value instanceof \DateTime) {
+                    return $value->format(\DateTime::ISO8601);
+                }
+
+                return 'object: ' . get_class($value);
+            } elseif (is_scalar($value)) {
+                return $value;
+            } elseif (is_array($value)) {
+                return 'array';
+            }
         }
     }
 }
